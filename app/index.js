9690b8ae08bfc6782d11377f85267016616ffda1
Nathan Raine <nraine18@gmail.com>
2020-05-15 16:36:50 -0500

    jsx support

--- app/index.js
+++ app/index.js
@@ -1,15 +1,7 @@
+import App from './components/App.jsx';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<App />,document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
