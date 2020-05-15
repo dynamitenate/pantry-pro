a60b6c6f96453d07a334ec4dbd25f2437360e0e9
Nathan Raine <nraine18@gmail.com>
2020-05-15 16:08:29 -0500

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + '/app/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.html'
+        })
+    ],
+    mode: 'none'
+};
