ab740bb1b4d3b46aec3cdb8a8722f501ade11daa
Nathan Raine <nraine18@gmail.com>
2020-05-23 18:49:17 -0500

    new command names

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
+const Express = require('express');
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(Express.static(__dirname + '/dist'));
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html');
 });
