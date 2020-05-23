926837107eec1becb782b28dfec323b4dd323d50
Nathan Raine <nraine18@gmail.com>
2020-05-23 18:05:12 -0500

    using import

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-
-const React = require('react');
-const ReactDOM = require('react-dom');
-require('./index.css');
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
