a6ca40135ccf63b4b1f71cb95242369f29447957
Adam Raine <theoperatorfiles@gmail.com>
2020-05-26 19:54:07 -0400

    Moved firebase config to independent file

--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ import ReactDOM from 'react-dom';
 import Firebase from 'firebase/app';
 import App from './components/App.jsx';
 import './index.css';
+import firebaseConfig from '../firebaseConfig.js'
 
-// Connect to Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyAkc1-_zeHsICFv6CaaxbjIVQNaCzvD1hg",
-    authDomain: "pantry-pro.firebaseapp.com",
-    databaseURL: "https://pantry-pro.firebaseio.com",
-    projectId: "pantry-pro",
-    storageBucket: "pantry-pro.appspot.com",
-    messagingSenderId: "305086027682",
-    appId: "1:305086027682:web:8651df290dc2f9233844fb"
-};
+// Connect to Firebase;
 Firebase.initializeApp(firebaseConfig);
 
 // Render root React component
