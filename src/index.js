72fa5ac5d553af15a505704c326671ba6b05e524
Nathan Raine <nraine18@gmail.com>
2020-05-27 21:28:20 -0500

    minor tweaks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import * as Firebase from 'firebase';
 import Firebase from 'firebase/app';
 import App from './components/App.jsx';
 import './index.css';
-const firebaseConfig = require('../firebaseConfig.json')
 
 // Connect to Firebase;
 Firebase.initializeApp(firebaseConfig);
+const firebaseConfig = require('../firebaseConfig.json')
 
 // Render root React component
 ReactDOM.render(
