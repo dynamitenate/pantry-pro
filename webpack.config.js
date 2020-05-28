663ab622027ccc860f3a267648767c53c4b62529
Nathan Raine <nraine18@gmail.com>
2020-05-28 17:20:30 -0500

    Webpack improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,50 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: __dirname + '/src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|svg)$/,
-                exclude: /node_modules/,
-                use: 'file-loader?name=[name].[ext]'
-            }
+module.exports = (options) => {
+    let isProduction;
+
+    if (options && options.mode === 'production') {
+        isProduction = true;
+    } else {
+        isProduction = false;
+    }
+
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: __dirname + '/src/index.js',
+        output: {
+            path: __dirname + '/dist',
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|svg)$/,
+                    exclude: /node_modules/,
+                    use: 'file-loader?name=[name].[ext]'
+                }
+            ]
+        },
+        devtool: isProduction ? undefined : 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: __dirname + '/src/index.html'
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html'
-        })
-    ],
-    mode: 'none'
+    };
 };
