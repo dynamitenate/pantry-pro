71e56e99f315bdb871c5bbdc99f5f1962f9d3214
Nathan Raine <nraine18@gmail.com>
2020-05-29 23:45:59 -0500

    improvements

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use('/spoonacular', require('./routes/spoonacular,js'));
 
 // Setup home directory
 app.use(express.static(__dirname + '/dist'));
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
+// Start server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
