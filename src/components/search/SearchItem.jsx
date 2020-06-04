9cc16c81ccc73906b08a7fd2d659de6e6c057896
Adam Raine <theoperatorfiles@gmail.com>
2020-06-04 18:56:11 -0400

    Button adding search result to shopping list

--- src/components/search/SearchItem.jsx
+++ src/components/search/SearchItem.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import Button from '../common/Button.jsx';
+import { withDatabase } from '../../hoc/FirebaseContext.jsx';
 
 class SearchItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
+    }
+
+    handleAddButtonClick() {
+        let table = this.props.db.ref().child("test-global-list");
+        table.once("value").then(snap => {
+            let items = snap.val().items;
+            items.push(this.props.value);
+            table.set({ items });
+        });
+    }
+
     render() {
         return (
             <div
                 className={'shopping-list-item'}
             >
+                <Button
+                    className={'shopping-list-add-button'}
+                    style={{
+                        width: 30,
+                        height: 30
+                    }}
+                    onClick={this.handleAddButtonClick}
+                >
+                </Button>
                 {this.props.value}
             </div>
         );
     }
 }
 
-export default SearchItem;
+export default withDatabase(SearchItem);
