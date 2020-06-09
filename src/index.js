24a3762f411b11c9dfc9c7740c437aa744563fbc
Nathan Raine <nraine18@gmail.com>
2020-06-09 00:01:26 -0500

    full routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 import './index.css';
 
 // Render root React component
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
     document.getElementById('app')
 );
